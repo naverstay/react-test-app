efd935df67ae810947d589449071091745117512
nv <naverstay.me@gmail.com>
2020-02-22 12:17:12 +0200

    Updates

---
+++ precache-manifest.83f79716b377166b50ef95dcd6c79b35.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7902d16d9ebbdf915fc9de54b959dfc7",
+    "url": "/react-test-app/index.html"
+  },
+  {
+    "revision": "d4ac342baf945a7a455f",
+    "url": "/react-test-app/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "8af1c97d36f0b1426ae8",
+    "url": "/react-test-app/static/js/2.b6fabe4d.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-test-app/static/js/2.b6fabe4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4ac342baf945a7a455f",
+    "url": "/react-test-app/static/js/main.774daf3a.chunk.js"
+  },
+  {
+    "revision": "2ff4ba167e12c79d721a",
+    "url": "/react-test-app/static/js/runtime-main.317a7e40.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
